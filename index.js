67025225b764d5ca22145acef4633e949db2dddb
antondavidenko <anton.davidenko@gmail.com>
2026-05-07 14:21:49 +0300

    feat(): store and retrive demo mode from cookies; try to detect mode automaticaly if nothing n cookies;

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ICO_FOLDER = "./images/content/";
 this.initIndexScripts = () => {
   this.loadContent("./content.json", () => {
     this.showContent();
-    this.switchMode('desktop');
+    this.switchMode(this.getDefaultMode());
   });
   document.getElementById("mobile-selector").addEventListener('click', () => this.switchMode('desktop'));
   document.getElementById("desktop-selector").addEventListener('click', () => this.switchMode('mobile'));
@@ -36,13 +36,9 @@ this.showItem = (itemJSON, id) => {
 
 this.switchMode = (mode) => {
   this.mode = mode;
-  let selectedSelector = document.getElementById("desktop-selector");
-  let unselectedSelector = document.getElementById("mobile-selector");
-  
-  if (mode !== 'desktop') {
-    selectedSelector = document.getElementById("mobile-selector");
-    unselectedSelector = document.getElementById("desktop-selector");
-  }
+  document.cookie = `mode=${mode}; path=/; max-age=31536000`;
+  const selectedSelector = document.getElementById(mode === 'desktop' ? "desktop-selector" : "mobile-selector");
+  const unselectedSelector = document.getElementById(mode === 'desktop' ? "mobile-selector" : "desktop-selector");
 
   unselectedSelector.style.display = "none";
   selectedSelector.style.display = "block";
@@ -79,4 +75,20 @@ this.showGame = (game, size) => {
   viewer.style.display = "block";
   document.getElementById("content").style.display = "none";
   document.getElementById("device-selector").style.display = "none";
+}
+
+this.getDefaultMode = () => {
+  const cookies = document.cookie.split(';').reduce((cookies, cookie) => {
+    const [name, value] = cookie.trim().split('=');
+    cookies[name] = value;
+    return cookies;
+  }, {});
+
+  if (cookies.mode) {
+    console.warn("Found mode in cookies:", cookies.mode);
+    return cookies.mode;
+  } else {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    return isMobile ? 'mobile' : 'desktop';
+  }
 }
